11364c6b5f75fb66400497e916662f229a96a804
setiyono ressly <setiyono.ressly@gmail.com>
2023-11-18 14:16:28 +0700

    create provider

---
+++ simple/simple.go
@@ -0,0 +1,16 @@
+package simple
+
+type SimpleRepository struct {
+}
+
+func NewSimpleRepository() *SimpleRepository {
+	return &SimpleRepository{}
+}
+
+type SimpleService struct {
+	*SimpleRepository
+}
+
+func NewSimpleService(repository *SimpleRepository) *SimpleService {
+	return &SimpleService{SimpleRepository: repository}
+}
